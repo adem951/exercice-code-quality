0690a55bc5f266140564cc3892d48e318a1cc289
adem951 <100589825+adem951@users.noreply.github.com>
2025-11-19 12:25:32 +0100

    Add all changes

---
+++ tests/test_integration_example.py
@@ -0,0 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import app
+
+def test_index_endpoint():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'<html' in response.data
